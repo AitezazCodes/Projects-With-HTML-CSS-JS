4db63ea6c459a4c41d1c00c1d4675165e89bf51f
ZazCodes <zazcodes@gmail.com>
2024-01-26 19:47:32 +0500

    completed Drag And Drop Project

--- Drag And Drop/script.js
+++ Drag And Drop/script.js
@@ -1,3 +1,27 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const rectang = document.querySelector(".rect");
+const leftBox = document.querySelector(".left");
+const rightBox = document.querySelector(".right");
+const DragElements = document.querySelectorAll(".rect");
+
+for (elem of DragElements) {
+    elem.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected);
+            selected = null;
+        });
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected);
+            selected = null;
+        });
+    });
+}
