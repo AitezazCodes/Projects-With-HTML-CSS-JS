9356d497422645754800ce79c8e41ae0bad1e989
ZazCodes <zazcodes@gmail.com>
2024-01-28 16:21:49 +0500

    started timer project

---
+++ Pomodoro Timer/script.js
@@ -0,0 +1,18 @@
+let time = document.getElementById("time");
+let Start = document.getElementById("start");
+let Stop = document.getElementById("stop");
+let Reset = document.getElementById("reset");
+
+let interval = 1500;
+
+Start.addEventListener("click", function(){
+    
+});
+
+Stop.addEventListener("click", function(){
+
+});
+
+Reset.addEventListener("click", function(){
+
+});
