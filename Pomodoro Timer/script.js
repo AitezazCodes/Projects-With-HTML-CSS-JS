819b71ac33f209bb119014d92e97a56935c6ec3b
ZazCodes <zazcodes@gmail.com>
2024-02-02 18:22:00 +0500

    added pomodoro counter

--- Pomodoro Timer/script.js
+++ Pomodoro Timer/script.js
@@ -3,16 +3,42 @@ let Start = document.getElementById("start");
 let Stop = document.getElementById("stop");
 let Reset = document.getElementById("reset");
 
-let interval = 1500;
+let interval;
+let timeLeft = 1500;
 
-Start.addEventListener("click", function(){
-    
-});
+function updateTimer(){
+    let min = Math.floor(timeLeft / 60);
+    let sec = timeLeft % 60;
 
-Stop.addEventListener("click", function(){
+    let formatTime = `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`
 
-});
+    time.innerHTML = formatTime;
+}
 
-Reset.addEventListener("click", function(){
+function startTimer(){
+    interval = setInterval(() =>{
+        timeLeft--;
+        updateTimer();
+        if(timeLeft === 0){
+            clearInterval(interval);
+            alert("Time's UP")
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000);
+}
 
-});
+function stopTimer(){
+    clearInterval(interval);
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+
+Start.addEventListener("click", startTimer);
+Stop.addEventListener("click", stopTimer);
+Reset.addEventListener("click", resetTimer);
